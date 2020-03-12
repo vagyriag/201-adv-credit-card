e47d70b1fc16b9d7aa1eb748a212e620412917c2
Sebastian Gaviria Giraldo <1112478378@academico.icesi.edu.co>
2020-03-12 17:01:31 -0500

    router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React from 'react';
 import './App.css';
 import CreditCard from './CreditCard/CreditCard';
 import Will from './Will/Will';
+import { HashRouter, Route, Link } from 'react-router-dom';
 
 function App() {
   
   return (
     <div className="App">
+      <HashRouter>
+        <Link className="link" to="/">Ir a La Home</Link>
+        <Link className="link" to="/will">Ir a Will</Link>
+        <Link className="link" to="/credit">Ir a CreditCard</Link>
+      
+        <Route path="/will" component={Will} />
 
-      <Will />
+        <Route path="/credit" component={CreditCard} />
 
-      <CreditCard />
+        <Route path="/" exact render={() => {
+          return <div>
+            <h1>La Home</h1>
+          </div>
+        }} />
+
+      </HashRouter>
     </div>
   );
 }
